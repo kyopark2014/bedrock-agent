a781ff0493466ce3061d8253c254b768566c6f2a
John Park <kyopark2014@gmail.com>
2024-12-28 01:38:55 +0900

    update

--- cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
+++ cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
@@ -406,8 +406,8 @@ export class CdkBedrockAgentStack extends cdk.Stack {
     // );    
     
     // deploy components
-    //new componentDeployment(scope, `component-deployment-of-${projectName}`, 
-    //  ec2Sg, albSg, accountId, knowledge_base_role, collectionArn, OpenSearchCollection, s3Bucket, vpc, ec2Role, alb)
+    new componentDeployment(scope, `component-deployment-of-${projectName}`, 
+      ec2Sg, albSg, accountId, knowledge_base_role, collectionArn, OpenSearchCollection, s3Bucket, vpc, ec2Role, alb)
   }
 }
 
