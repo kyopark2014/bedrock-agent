130460abbd3d06dab0369006e58fdd273d9ffc39
John Park <kyopark2014@gmail.com>
2024-12-27 19:07:05 +0900

    update

--- cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
+++ cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
@@ -6,16 +6,182 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import * as elbv2_tg from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets'
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as opensearchserverless from 'aws-cdk-lib/aws-opensearchserverless';
 
 const projectName = `bedrock-agent`; 
 const region = process.env.CDK_DEFAULT_REGION;    
 const accountId = process.env.CDK_DEFAULT_ACCOUNT;
 const targetPort = 8080;
 const bucketName = `storage-for-${projectName}-${accountId}-${region}`; 
+const vectorIndexName = projectName
 
 export class CdkBedrockAgentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // Knowledge Base Role
+    const knowledge_base_role = new iam.Role(this,  `role-knowledge-base-for-${projectName}`, {
+      roleName: `role-knowledge-base-for-${projectName}-${region}`,
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal("bedrock.amazonaws.com")
+      )
+    });
+    
+    const bedrockInvokePolicy = new iam.PolicyStatement({ 
+      effect: iam.Effect.ALLOW,
+      resources: [
+        `arn:aws:bedrock:${region}::foundation-model/anthropic.claude-3-haiku-20240307-v1:0`,
+        `arn:aws:bedrock:${region}::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0`,
+        `arn:aws:bedrock:${region}::foundation-model/amazon.titan-embed-text-v1`,
+        `arn:aws:bedrock:${region}::foundation-model/amazon.titan-embed-text-v2:0`
+      ],
+      // resources: ['*'],
+      actions: [
+        "bedrock:InvokeModel", 
+        "bedrock:InvokeModelEndpoint", 
+        "bedrock:InvokeModelEndpointAsync",        
+      ],
+    });        
+    knowledge_base_role.attachInlinePolicy( 
+      new iam.Policy(this, `bedrock-invoke-policy-for-${projectName}`, {
+        statements: [bedrockInvokePolicy],
+      }),
+    );  
+    
+    const bedrockKnowledgeBaseS3Policy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:ListMultipartUploadParts",
+        "s3:AbortMultipartUpload",
+        "s3:CreateBucket",
+        "s3:PutObject",
+        "s3:PutBucketLogging",
+        "s3:PutBucketVersioning",
+        "s3:PutBucketNotification",
+      ],
+    });
+    knowledge_base_role.attachInlinePolicy( 
+      new iam.Policy(this, `knowledge-base-s3-policy-for-${projectName}`, {
+        statements: [bedrockKnowledgeBaseS3Policy],
+      }),
+    );  
+    
+    const knowledgeBaseOpenSearchPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: ["aoss:APIAccessAll"],
+    });
+    knowledge_base_role.attachInlinePolicy( 
+      new iam.Policy(this, `bedrock-agent-opensearch-policy-for-${projectName}`, {
+        statements: [knowledgeBaseOpenSearchPolicy],
+      }),
+    );  
+
+    const knowledgeBaseBedrockPolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: ["bedrock:*"],
+    });
+    knowledge_base_role.attachInlinePolicy( 
+      new iam.Policy(this, `bedrock-agent-bedrock-policy-for-${projectName}`, {
+        statements: [knowledgeBaseBedrockPolicy],
+      }),
+    );  
+
+    // OpenSearch Serverless
+    const collectionName = vectorIndexName
+    const OpenSearchCollection = new opensearchserverless.CfnCollection(this, `opensearch-correction-for-${projectName}`, {
+      name: collectionName,    
+      description: `opensearch correction for ${projectName}`,
+      standbyReplicas: 'DISABLED',
+      type: 'VECTORSEARCH',
+    });
+    const collectionArn = OpenSearchCollection.attrArn
+    const opensearch_url = OpenSearchCollection.attrCollectionEndpoint
+
+    new cdk.CfnOutput(this, 'OpensearchCollectionEndpoint', {
+      value: opensearch_url,
+      description: 'The endpoint of opensearch correction',
+    });
+
+    const encPolicyName = `encription-${projectName}`
+    const encPolicy = new opensearchserverless.CfnSecurityPolicy(this, `opensearch-encription-security-policy-for-${projectName}`, {
+      name: encPolicyName,
+      type: "encryption",
+      description: `opensearch encryption policy for ${projectName}`,
+      policy:
+        '{"Rules":[{"ResourceType":"collection","Resource":["collection/*"]}],"AWSOwnedKey":true}',      
+    });
+    OpenSearchCollection.addDependency(encPolicy);
+
+    const netPolicyName = `network-${projectName}`
+    const netPolicy = new opensearchserverless.CfnSecurityPolicy(this, `opensearch-network-security-policy-for-${projectName}`, {
+      name: netPolicyName,
+      type: 'network',    
+      description: `opensearch network policy for ${projectName}`,
+      policy: JSON.stringify([
+        {
+          Rules: [
+            {
+              ResourceType: "dashboard",
+              Resource: [`collection/${collectionName}`],
+            },
+            {
+              ResourceType: "collection",
+              Resource: [`collection/${collectionName}`],              
+            }
+          ],
+          AllowFromPublic: true,          
+        },
+      ]), 
+      
+    });
+    OpenSearchCollection.addDependency(netPolicy);
+
+    const dataAccessPolicyName = `data-${projectName}`
+    const dataAccessPolicy = new opensearchserverless.CfnAccessPolicy(this, `opensearch-data-collection-policy-for-${projectName}`, {
+      name: dataAccessPolicyName,
+      type: "data",
+      policy: JSON.stringify([
+        {
+          Rules: [
+            {
+              Resource: [`collection/${collectionName}`],
+              Permission: [
+                "aoss:CreateCollectionItems",
+                "aoss:DeleteCollectionItems",
+                "aoss:UpdateCollectionItems",
+                "aoss:DescribeCollectionItems",
+              ],
+              ResourceType: "collection",
+            },
+            {
+              Resource: [`index/${collectionName}/*`],
+              Permission: [
+                "aoss:CreateIndex",
+                "aoss:DeleteIndex",
+                "aoss:UpdateIndex",
+                "aoss:DescribeIndex",
+                "aoss:ReadDocument",
+                "aoss:WriteDocument",
+              ], 
+              ResourceType: "index",
+            }
+          ],
+          Principal: [
+            `arn:aws:iam::${accountId}:role/${knowledge_base_role.roleName}`,
+            `arn:aws:iam::${accountId}:role/role-lambda-chat-ws-for-${projectName}-${region}`,
+            //`arn:aws:iam::${accountId}:role/administration`,
+            `arn:aws:sts::${accountId}:assumed-role/administration/ksdyb-Isengard`, 
+          ], 
+        },
+      ]),
+    });
+    OpenSearchCollection.addDependency(dataAccessPolicy);
 
     // s3 
     const s3Bucket = new s3.Bucket(this, `storage-${projectName}`,{
