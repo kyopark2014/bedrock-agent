ddcf28418861dd6be25ca01b5bb74762243ebcd7
John Park <kyopark2014@gmail.com>
2024-12-28 11:35:58 +0900

    update

--- cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
+++ cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
@@ -477,7 +477,7 @@ port=${targetPort}`
     });
 
     // EC2 instance
-    const appInstance = new ec2.Instance(this, `app-for-${projectName}`, {
+ /*   const appInstance = new ec2.Instance(this, `app-for-${projectName}`, {
       instanceName: `app-for-${projectName}`,
       instanceType: new ec2.InstanceType('t2.small'), // m5.large
       // instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
@@ -519,6 +519,6 @@ port=${targetPort}`
       targets,
       protocol: elbv2.ApplicationProtocol.HTTP,
       port: targetPort
-    });           
+    });        */   
   }
 }
