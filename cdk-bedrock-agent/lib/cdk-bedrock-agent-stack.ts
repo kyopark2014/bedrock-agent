05292edf8f93dfb29f7beaacce601022354e51d4
John Park <kyopark2014@gmail.com>
2024-12-28 12:44:21 +0900

    update

--- cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
+++ cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
@@ -451,7 +451,7 @@ port=${targetPort}`
       `runuser -l ec2-user -c "mkdir -p /home/ec2-user/.streamlit"`,
       `runuser -l ec2-user -c "cat <<EOF > /home/ec2-user/.streamlit/config.toml\n${config_toml}EOF"`,
       `sh -c "cat <<EOF > /etc/systemd/system/streamlit.service\n${streamlit_service}EOF"`,
-      `runuser -l ec2-user -c "cat <<EOF > /home/ec2-user/.env\n${environment_variables}\nEOF"`,
+      `runuser -l ec2-user -c 'echo "${environment_variables}" >> /home/ec2-user/env.sh'`,
       `runuser -l ec2-user -c 'cd && git clone https://github.com/kyopark2014/${projectName}'`,
       `runuser -l ec2-user -c 'pip install streamlit streamlit_chat beautifulsoup4 pytz tavily-python'`,        
       `runuser -l ec2-user -c 'pip install boto3 langchain_aws langchain langchain_community langgraph opensearch-py'`,                 
@@ -459,7 +459,7 @@ port=${targetPort}`
       'systemctl start streamlit'
     ];
     userData.addCommands(...commands);
-
+    
     // EC2 instance
     const appInstance = new ec2.Instance(this, `app-for-${projectName}`, {
       instanceName: `app-for-${projectName}`,
@@ -503,6 +503,6 @@ port=${targetPort}`
       targets,
       protocol: elbv2.ApplicationProtocol.HTTP,
       port: targetPort
-    });          
+    });           
   }
 }
