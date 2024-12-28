3b60dd95f22c9a760a8beb99ac63b8968ed3eacd
John Park <kyopark2014@gmail.com>
2024-12-28 10:46:38 +0900

    update

--- cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
+++ cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
@@ -407,7 +407,7 @@ export class CdkBedrockAgentStack extends cdk.Stack {
 
     const userData = ec2.UserData.forLinux();
 
-    const environment = `
+    const environment_variables = `
 export projectName=${projectName}
 export accountId=${accountId}
 export region=${region}
@@ -417,7 +417,7 @@ export opensearch_url=${OpenSearchCollection.attrCollectionEndpoint}
 export s3_arn=${s3Bucket.bucketArn}`
     
     new cdk.CfnOutput(this, `environment-for-${projectName}`, {
-      value: environment,
+      value: environment_variables,
       description: `environment-${projectName}`,
       exportName: `environment-${projectName}`
     });
@@ -447,7 +447,7 @@ port=${targetPort}`
       'pip install pip --upgrade',            
       `runuser -l ec2-user -c 'pip install watchtower'`,  // debug 
       `sh -c "cat <<EOF > /etc/systemd/system/streamlit.service\n${streamlit_service}EOF"`,
-      `runuser -l ec2-user -c '${environment}'`,
+      `runuser -l ec2-user -c '${environment_variables}'`,
       `runuser -l ec2-user -c "mkdir -p /home/ec2-user/.streamlit"`,
       `runuser -l ec2-user -c "cat <<EOF > /home/ec2-user/.streamlit/config.toml\n${config_toml}EOF"`,
       `runuser -l ec2-user -c 'cd && git clone https://github.com/kyopark2014/${projectName}'`,
@@ -457,13 +457,6 @@ port=${targetPort}`
       'systemctl start streamlit'
     ];
     
-    userData.addCommands(...commands);
-    new cdk.CfnOutput(this, `userDataCommand-for-${projectName}`, {
-      value: `runuser -l ec2-user -c '${environment}'`,
-      description: `userDataCommand-${projectName}`,
-      exportName: `userDataCommand-${projectName}`
-    });    
-
     new cdk.CfnOutput(this, `KnowledgeBaseRole-for-${projectName}`, {
       value: knowledge_base_role.roleArn,
       description: `KnowledgeBaseRole-${projectName}`,
