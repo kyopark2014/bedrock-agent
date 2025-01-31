b585e1482ce710f34c491ad816c5de8f62defd49
John Park <kyopark2014@gmail.com>
2025-01-31 19:39:15 +0900

    update

--- cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
+++ cdk-bedrock-agent/lib/cdk-bedrock-agent-stack.ts
@@ -521,7 +521,7 @@ export class CdkBedrockAgentStack extends cdk.Stack {
     );  
 
     // lambda-tool
-   /* const roleLambdaTools = new iam.Role(this, `role-lambda-tools-for-${projectName}`, {
+    const roleLambdaTools = new iam.Role(this, `role-lambda-tools-for-${projectName}`, {
       roleName: `role-lambda-tools-for-${projectName}-${region}`,
       assumedBy: new iam.CompositePrincipal(
         new iam.ServicePrincipal("lambda.amazonaws.com"),
@@ -564,7 +564,7 @@ export class CdkBedrockAgentStack extends cdk.Stack {
     lambdaTools.addPermission(`lambda-tools-permission-for-${projectName}`, {      
       principal: new iam.ServicePrincipal('bedrock.amazonaws.com'),
       action: 'lambda:InvokeFunction'
-    }) */
+    })
 
     // user data for setting EC2
     const userData = ec2.UserData.forLinux();
@@ -580,7 +580,7 @@ export class CdkBedrockAgentStack extends cdk.Stack {
       "s3_bucket": s3Bucket.bucketName,      
       "s3_arn": s3Bucket.bucketArn,
       "sharing_url": 'https://'+distribution_sharing.domainName,
-    //  "lambda-tools": lambdaTools.functionArn
+      "lambda-tools": lambdaTools.functionArn
     }    
     new cdk.CfnOutput(this, `environment-for-${projectName}`, {
       value: JSON.stringify(environment),
